ac93f6c8a8bf4b3da64077643ccc719d59f136f4
richardjpurcell <richardjpurcell@yahoo.com>
2020-06-01 15:22:09 -0300

    Added Face Detection

--- sunGlassesFilter/Source.cpp
+++ sunGlassesFilter/Source.cpp
@@ -10,9 +10,32 @@
 
 using namespace cv;
 using namespace std;
+using namespace cv::dnn;
+
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const double inScaleFactor = 1.0;
+const float confidenceThreshold = 0.7;
+const cv::Scalar meanVal(104.0, 177.0, 123.0);
+
+#define CAFFE
+
+const std::string caffeConfigFile = "./data/models/deploy.prototxt";
+const std::string caffeWeightFile = "./data/models/res10_300x300_ssd_iter_140000_fp16.caffemodel";
+
+const std::string tensorflowConfigFile = "./data/models/opencv_face_detector.pbtxt";
+const std::string tensorflowWeightFile = "./data/models/opencv_face_detector_uint8.pb";
+
+void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN);
 
 int main(int argc, const char** argv)
 {
+#ifdef CAFFE
+    Net net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
+#else
+    Net net = cv::dnn::readNetFromTensorflow(tensorflowWeightFile, tensorflowConfigFile);
+#endif
+
     Mat frame;
 
     VideoCapture cap;
@@ -28,6 +51,9 @@ int main(int argc, const char** argv)
         return -1;
     }
 
+    double tt_opencvDNN = 0;
+    double fpsOpencvDNN = 0;
+
     cout << "start grabbing frames" << endl
         << "Press any key to terminate" << endl;
     for (;;)
@@ -40,12 +66,48 @@ int main(int argc, const char** argv)
             break;
         }
 
-        imshow("Live", frame);
+        double t = cv::getTickCount();
+        detectFaceOpenCVDNN(net, frame);
+        tt_opencvDNN = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        fpsOpencvDNN = 1 / tt_opencvDNN;
+        
+        imshow("Sunglass Cam", frame);
         if (waitKey(5) >= 0)
             break;
     }
 
-
     return 0;
+}
+
+void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN)
+{
+    int frameHeight = frameOpenCVDNN.rows;
+    int frameWidth = frameOpenCVDNN.cols;
+#ifdef CAFFE
+    cv::Mat inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, false, false);
+#else
+    cv::Mat inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, true, false);
+#endif
+
+    net.setInput(inputBlob, "data");
+    cv::Mat detection = net.forward("detection_out");
+
+    cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+    for (int i = 0; i < detectionMat.rows; i++)
+    {
+        float confidence = detectionMat.at<float>(i, 2);
+
+        if (confidence > confidenceThreshold)
+        {
+            int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
+            int y1 = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
+            int x2 = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
+            int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
+
+            cv::rectangle(frameOpenCVDNN, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2, 4);
+        }
+    }
+
 }
 
