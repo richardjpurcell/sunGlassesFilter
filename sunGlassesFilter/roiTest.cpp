1dae8b5ac9b8a64f9af0d2acda571e178703e33b
richardjpurcell <richardjpurcell@yahoo.com>
2020-06-03 15:17:29 -0300

    Resize and translate of sunglasses test function complete.

--- sunGlassesFilter/roiTest.cpp
+++ sunGlassesFilter/roiTest.cpp
@@ -12,9 +12,12 @@ using namespace std;
 
 void alphaBlend(Mat& foreground, Mat& background, Mat& alpha, Mat& outImage)
 {
+    //get rid of roi information in header
+    Mat temp = background.clone();
+    //create pointers to Mat arrays
     int numberOfPixels = foreground.rows * foreground.cols * foreground.channels();
     float* fptr = reinterpret_cast<float*>(foreground.data);
-    float* bptr = reinterpret_cast<float*>(background.data);
+    float* bptr = reinterpret_cast<float*>(temp.data);
     float* aptr = reinterpret_cast<float*>(alpha.data);
     float* outPtr = reinterpret_cast<float*>(outImage.data);
 
@@ -24,32 +27,42 @@ void alphaBlend(Mat& foreground, Mat& background, Mat& alpha, Mat& outImage)
     }
 }
 
+void drawRect(Mat& img, Point& point1, Point& point2, bool showRect)
+{
+    Mat temp = img.clone();
+    rectangle(img, point1, point2, Scalar(255, 255, 0), 4);
+}
+
 int main()
 {
-    Mat foreground = imread("sunglasses.jpg");
+    //images
+    Mat foreground = imread("sunglasses_foreground.jpg");
     Mat background = imread("ocean.png");
     Mat alpha = imread("sunglasses_alpha.jpg");
-
-    int height = foreground.rows;
-    int width = foreground.cols;
-    resize(foreground, foreground, Size(width / 2, height / 2), INTER_CUBIC);
-    resize(alpha, alpha, Size(width / 2, height / 2), INTER_CUBIC);
-
+    //bounding rectangle
+    Point point1 = Point(100, 100);
+    Point point2 = Point(300, 600);
+    //calculate sunglasses resize
+    int newWidth = point2.x - point1.x;
+    int newHeight = (newWidth * foreground.rows)/foreground.cols;
+    //resize foreground and alpha to bounding rectangle
+    resize(foreground, foreground, Size(newWidth, newHeight), INTER_CUBIC);
+    resize(alpha, alpha, Size(newWidth, newHeight), INTER_CUBIC);
+    //for testing, draw rectangle
+    drawRect(background, point1, point2, true);
+    //convert to 32 bit
     foreground.convertTo(foreground, CV_32FC3);
     background.convertTo(background, CV_32FC3);
     alpha.convertTo(alpha, CV_32FC3, 1.0 / 255);
-
     Mat outImage = Mat::zeros(foreground.size(), foreground.type());
-
-    cout << foreground.size() << endl;
-    cout << background.size() << endl;
-
-    Rect roi = Rect(0, 0, foreground.cols, foreground.rows);
+    //prepare region of interest
+    Rect roi = Rect(point1.x, point1.y + (point2.y - point1.y) / 3, newWidth, newHeight);
     Mat background_roi = background(roi);
-
+    //blend the images together
     alphaBlend(foreground, background_roi, alpha, outImage);
-
-    imshow("alpha blended", outImage / 255);
+    //recombine bg and bg_roi
+    outImage.copyTo(background(roi));
+    imshow("background", background / 255);
     waitKey(0);
 
     return 0;
