a88abfc090c3784664d3e96b8b0a5239b98c5272
richardjpurcell <richardjpurcell@yahoo.com>
2020-06-22 11:59:13 -0300

    Sunglass tracking working.

--- sunGlassesFilter/Source.cpp
+++ sunGlassesFilter/Source.cpp
@@ -7,16 +7,22 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/dnn.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 using namespace cv;
 using namespace std;
 using namespace cv::dnn;
 
+#define BLUR 15 //must be odd number
+
 const size_t inWidth = 300;
 const size_t inHeight = 300;
 const double inScaleFactor = 1.0;
-const float confidenceThreshold = 0.7;
+const float confidenceThreshold = 0.5;
 const cv::Scalar meanVal(104.0, 177.0, 123.0);
+Mat sunglassesRGB = imread("sunglasses_foreground.jpg");
+Mat sunglassesA = imread("sunglasses_alpha.jpg");
 
 #define CAFFE
 
@@ -28,58 +34,74 @@ const std::string tensorflowWeightFile = "./data/models/opencv_face_detector_uin
 
 void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN);
 
-int main(int argc, const char** argv)
+void alphaBlend(Mat& foreground, Mat& background, Mat& alpha, Mat& outImage)
 {
-#ifdef CAFFE
-    Net net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
-#else
-    Net net = cv::dnn::readNetFromTensorflow(tensorflowWeightFile, tensorflowConfigFile);
-#endif
+    //get rid of roi information in header
+    Mat temp = background.clone();
+    //create pointers to Mat arrays
+    int numberOfPixels = foreground.rows * foreground.cols * foreground.channels();
+    float* fptr = reinterpret_cast<float*>(foreground.data);
+    float* bptr = reinterpret_cast<float*>(temp.data);
+    float* aptr = reinterpret_cast<float*>(alpha.data);
+    float* outPtr = reinterpret_cast<float*>(outImage.data);
 
-    Mat frame;
-
-    VideoCapture cap;
-
-    int deviceID = 0;
-    int apiID = cv::CAP_ANY;
-
-    cap.open(deviceID, apiID);
-
-    if (!cap.isOpened())
+    for (int i = 0; i < numberOfPixels; i++, outPtr++, fptr++, bptr++, aptr++)
     {
-        cerr << "ERROR! Unable to open camera\n";
-        return -1;
+        *outPtr = (*fptr) * (*aptr) + (*bptr) * (1 - *aptr);
     }
-
-    double tt_opencvDNN = 0;
-    double fpsOpencvDNN = 0;
-
-    cout << "start grabbing frames" << endl
-        << "Press any key to terminate" << endl;
-    for (;;)
-    {
-        cap.read(frame);
-
-        if (frame.empty())
-        {
-            cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-
-        double t = cv::getTickCount();
-        detectFaceOpenCVDNN(net, frame);
-        tt_opencvDNN = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-        fpsOpencvDNN = 1 / tt_opencvDNN;
-        
-        imshow("Sunglass Cam", frame);
-        if (waitKey(5) >= 0)
-            break;
-    }
-
-    return 0;
 }
 
-void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN)
+void drawRect(Mat& img, Point& point1, Point& point2, bool showRect)
+{
+    Mat temp = img.clone();
+    rectangle(img, point1, point2, Scalar(255, 255, 0), 4);
+}
+
+void sunglassesProcess(Mat& foreground, Mat& alpha, Point* points)
+{
+    foreground = sunglassesRGB.clone();
+    alpha = sunglassesA.clone();
+
+    //calculate sunglasses resize
+    int newWidth = points[1].x - points[0].x;
+    int newHeight = (newWidth * foreground.rows) / foreground.cols;
+    //preblur foreground and alpha to help resize look less jaggy
+    GaussianBlur(foreground, foreground, Size(BLUR, BLUR), 0, 0);
+    GaussianBlur(alpha, alpha, Size(BLUR, BLUR), 0, 0);
+    //resize foreground and alpha to bounding rectangle
+
+    if ((points[1].x - points[0].x) > 0)
+    {
+        resize(foreground, foreground, Size(newWidth, newHeight), INTER_AREA);
+        resize(alpha, alpha, Size(newWidth, newHeight), INTER_CUBIC);
+    }
+}
+
+void blendImages(Mat& foreground, Mat& background, Mat& alpha, Mat& out, Point* points)
+{
+    //calculate sunglasses resize
+    int newWidth = points[1].x - points[0].x;
+    int newHeight = (newWidth * foreground.rows) / foreground.cols;
+    //convert to 32 bit
+    foreground.convertTo(foreground, CV_32FC3);
+    background.convertTo(background, CV_32FC3);
+    alpha.convertTo(alpha, CV_32FC3, 1.0 / 255);
+
+    if ((points[1].x - points[0].x) > 0)
+    {
+        out = Mat::zeros(foreground.size(), foreground.type());
+        //prepare region of interest
+        Rect roi = Rect(points[0].x, points[0].y + 2 * (points[1].y - points[0].y) / 7, newWidth, newHeight);
+        Mat background_roi = background(roi);
+        //blend the images together
+        alphaBlend(foreground, background_roi, alpha, out);
+        //recombine bg and bg_roi
+        out.copyTo(background(roi));
+        //imshow("Sunglass Cam", background / 255);
+    }
+}
+
+void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN, Point *points)
 {
     int frameHeight = frameOpenCVDNN.rows;
     int frameWidth = frameOpenCVDNN.cols;
@@ -100,14 +122,72 @@ void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN)
 
         if (confidence > confidenceThreshold)
         {
-            int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
-            int y1 = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
-            int x2 = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
-            int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
-
-            cv::rectangle(frameOpenCVDNN, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2, 4);
+            points[0].x = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
+            points[0].y = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
+            points[1].x = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
+            points[1].y = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
         }
     }
 
 }
+
+int main(int argc, const char** argv)
+{
+#ifdef CAFFE
+    Net net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
+#else
+    Net net = cv::dnn::readNetFromTensorflow(tensorflowWeightFile, tensorflowConfigFile);
+#endif
+
+    //images
+    Mat foreground = sunglassesRGB.clone();
+    Mat alpha = sunglassesA.clone();
+    Mat frame;
+    Mat out;
+    //camera setup
+    VideoCapture cap;
+    int deviceID = 0;
+    int apiID = cv::CAP_ANY;
+    cap.open(deviceID, apiID);
+    if (!cap.isOpened())
+    {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    //DNN setup
+    double tt_opencvDNN = 0;
+    double fpsOpencvDNN = 0;
+    //corner points of face rectangle
+    Point points[2];
+
+    cout << "start grabbing frames" << endl
+        << "Press any key to terminate" << endl;
+    for (;;)
+    {
+        cap.read(frame);
+
+        if (frame.empty())
+        {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+
+        double t = cv::getTickCount();
+        detectFaceOpenCVDNN(net, frame, points);
+        //face rectangle for testing
+        //cv::rectangle(frame, points[0], points[1], cv::Scalar(0, 255, 0), 2, 4);
+        sunglassesProcess(foreground, alpha, points);
+        blendImages(foreground, frame, alpha, out, points);
+        tt_opencvDNN = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        fpsOpencvDNN = 1 / tt_opencvDNN;
+        
+        imshow("Sunglass Cam", frame / 255);
+        if (waitKey(5) >= 0)
+            break;
+    }
+
+    return 0;
+}
+
+
 
